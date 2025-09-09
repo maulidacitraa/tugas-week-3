0dc6950d8dc73a078a344dadb2358c72f9eaacc7
maulidacitraa <maulida.ellis@gmail.com>
2025-09-09 13:57:14 +0800

    Create percabangan.java

---
+++ Percabangan/percabangan.java
@@ -0,0 +1,11 @@
+public class percabangan {
+    void cekAngka(int x) {
+        if (x > 0) {
+            System.out.println(x + " adalah bilangan positif");
+        } else if (x < 0) {
+            System.out.println(x + " adalah bilangan negatif");
+        } else {
+            System.out.println("Angka adalah nol");
+        }
+    }
+}
